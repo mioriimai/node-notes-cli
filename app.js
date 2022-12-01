1d32354e154762511864d2597d646dfd8b0a1744
Parker Siu <parker.siu@gmail.com>
2022-12-01 18:52:13 +0000

    Completed update feature.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const data = require('./data.json');
 const command = process.argv[2];
 const string = process.argv[3];
+const newString = process.argv[4];
 
 if (command === 'read') {
   for(let key in data.notes) {
@@ -19,6 +20,14 @@ if (command === 'read') {
 } else if (command === 'delete') {
   delete data.notes[string];
   data.nextId--;
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), 'utf8', err => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  })
+} else if (command === 'update') {
+  data.notes[string] = newString;
   fs.writeFile('data.json', JSON.stringify(data, null, 2), 'utf8', err => {
     if (err) {
       console.error(err);

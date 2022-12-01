a5879ca73bd71642ac8dd32d51caaa7be2e16f75
Sara Choi <sarachoi10@hotmail.com>
2022-12-01 18:33:40 +0000

    updates

--- app.js
+++ app.js
@@ -4,11 +4,25 @@
 const fs = require('fs');
 const data = require('./data.json');
 const command = process.argv[2];
+const string = process.argv[3];
 
 if (command === 'read') {
   for(let key in data.notes) {
     console.log(`${key}: ${data.notes[key]}`);
   }
-}
+} else if (command === 'create') {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    });
+  fs.writeFile('data.json', string, 'utf8', err => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    data.notes.nextId = string;
+    data.nextId++;
+  })
 
-if (command === 'add') {}
+}
